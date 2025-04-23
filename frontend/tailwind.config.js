c7d73c1e8a58d606acbb64aeecb91f654c2fe314
AJ Ojeda <ajojeda@me.com>
2025-04-23 08:59:47 -0700

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   frontend/package-lock.json
    	modified:   frontend/package.json
    	new file:   frontend/postcss.config.cjs
    	deleted:    frontend/postcss.config.js
    	modified:   frontend/tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/**/*.{js,ts,jsx,tsx}", // optional depth if you nest deeply
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sidebar: "#2C2C2C",
+        sidebarText: "#E0E0E0",
+      },
+    },
   },
   plugins: [],
-}
+};
